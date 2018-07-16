552ff92e58ba0efcf0fd0560dadd490287b153c2
chuijse <cristian.huijse@gmail.com>
2018-07-16 19:28:16 -0400

    nuevo Home

--- blocks/home.php
+++ blocks/home.php
@@ -2,17 +2,73 @@
 <link rel="stylesheet" href="styles/style.css" type="text/css" />
 </head>
 
+<style>
+
+table{
+  padding: 20px;
+    
+}
+
+th{
+    
+	padding-right: 18px;
+	padding-left: 15px;
+}
+    
+#first{
+	border-right:3px dotted #808080;
+}	
+
+
+
+	
+
+</style>
+
 
 <article class="mid">
 	
-		<br>
-		<h2><FONT COLOR="#ff9955">Cristian Huijse Heise </FONT></h2>
-		<h3>Diseñador Industrial</h3>
+		<br><br><br>
 		<img src="images/yo.jpe" alt="" />
-		<p>Soy oriundo de Valdivia y estudié en la Universidad Diego Portales.<br>
-Tengo experiencia en proyectos de diseño de interfaces digitales físicas y virtuales, diseño estratégico, posicionamiento de marca, diseño sustentable y 		desarrollo de productos. Soy capaz de entenderme con profesionales de áreas afines tales como informática, electrónica e industrias.
-Estoy particularmente interesado en el desarrollo de proyectos del estado del arte de la interacción digital que requieran de trabajo investigativo y la docencia 	en estas áreas.
-		</p>
+		<br>
+		<table style="width:100%">
+				  <tr >
+					<th id="first" ALIGN=right VALIGN=top width="50%">
+					
+						<h4 ALIGN=right>ME</h4>
+						<p> 
+						<small>Nombre:</small><br> 
+						Cristian Javier Huijse Heise<br>
+						<br><small>Fecha de nacimiento:</small><br> 
+						Diseñador Industrial<br>
+						<br><small>Fecha de nacimiento:</small><br> 
+						5 de Noviembre de 1987<br> 
+						<br><small>email:</small><br> 
+						Cristian.Huijse@gmail.com<br>
+						</p>
+
+					</th>
+			
+				<th ALIGN=left VALIGN=top>
+					<h4 ALIGN=left>INTERESES</h4>
+				<p>
+				<b>Sculpting y modelado 3D,</b>
+				<small>arte generativo, </small>
+				<em>realidad aumentada,</em>
+				<b>electrónica,</b>
+				
+				<strong>computación física,</strong>
+				<small>fractales, </small>
+				<b>diseño de videojuegos,</b>
+				<em>software libre, </em>  
+				<strong>game engines,</strong> 
+				<small>andinismo, </small>
+				<em>instrumentos de cuerda y piano, </em>
+				<ins>diseño y fabricación de herramientas digitales CNC</ins>
+				</p>
+				</tr>
+			</th>
+		</table>		
 	
 	 
 </article>
