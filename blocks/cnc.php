f7a2814875c1dca8139a5e9946ffdb0f0aec97ea
chuijse <cristian.huijse@gmail.com>
2018-12-04 17:57:11 -0300

    arreglo general

---
+++ blocks/cnc.php
@@ -0,0 +1,48 @@
+<link rel="stylesheet" href="styles/style.css" type="text/css" />
+<script src="scripts/loadscnc.js" type="text/javascript"></script>
+
+
+<style>
+table.work {
+	
+	margin: auto;
+	padding: auto;
+	
+}
+table.work th{
+    padding: 5px;
+
+}
+</style>
+
+
+<article class="mid">
+	 
+	<br><br>
+	<table class="work">
+		
+			<nav>
+				<ul>
+					<tr>
+					<th><li ><span class="clickableind" id="3d"><img src="images/cnc/nav/work2.png"></span></li></th> 
+				
+					<th><li ><span class="clickableind" id="router"><img src="images/cnc/nav/work1.png"></span></li></th>
+					</tr> 
+				  
+				</ul>
+			</nav>
+		
+	</table>
+	<br>
+	
+	<div class="hrrel"></div>
+	
+	<div id="centerind"> 
+		<div id="waitingind"></div>
+		<div id="centercontentind"></div>
+		
+	</div>
+
+
+
+</article >
